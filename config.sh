fee2a39ee1ee9d1fa9819f336f8258a3bdaa9d7f
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-07 21:32:38 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Device
-export TWRP_BRANCH="twrp-11"
-export DT_LINK="https://github.com/Arafattex/ums512 -b in2b"
+export TWRP_BRANCH="Bg"
+export DT_LINK="https://github.com/Arafattex/S-Vendor-Twrp-Tree-for-mt6768 -b Lancelot_bot2"
 
-export DEVICE="E7544"
-export OEM="micromax"
+export DEVICE="lancelot"
+export OEM="xiaomi"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="bootimage"
+export TARGET="recoveryimage"
 
 export OUTPUT="Twrp*.zip"
 # Not Recommended to Change
