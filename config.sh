bfdb325780e8ff88ec24eb680ba30efd244df811
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-07 18:05:36 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export TWRP_BRANCH="twrp-12.1_11"
-export DT_LINK="https://github.com/Arafattex/S-Vendor-Twrp-Tree-for-mt6768 -b Lc"
+export TWRP_BRANCH="twrp-11"
+export DT_LINK="https://github.com/Arafattex/ums512 -b in2bt"
 
-export DEVICE="lancelot"
-export OEM="xiaomi"
+export DEVICE="E7544"
+export OEM="micromax"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -14,7 +14,6 @@ export OEM="xiaomi"
 export TARGET="recoveryimage"
 
 export OUTPUT="Twrp*.zip"
-
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
 export USE_CCACHE=1
