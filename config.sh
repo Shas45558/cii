a036309c845c4f12b8d2d3b35aba2a2de0fbec63
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-08 07:13:30 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/Arafattex/S-Vendor-Twrp-Tree-for-mt6768.git -b Lancelot_ofox"
+export FOX_BRANCH="fox_11"
+export DT_LINK="https://github.com/Arafattex/ums512.git -b in2b"
 
-export DEVICE="lancelot"
-export OEM="xiaomi"
+export DEVICE="E7544"
+export OEM="micromax"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
@@ -26,7 +26,7 @@ export EXTRA_CMD="export OF_MAINTAINER=@shas45558"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
-export OF_USE_LATEST_MAGISK=true
+export OF_USE_LATEST_MAGISK=false
 
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
