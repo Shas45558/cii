c30f85fa626af021d0ccedb5b1e8d93c1fd12e16
Arafattex <you@example.com>
2022-10-11 07:24:35 +0600

    x

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -15,19 +15,11 @@ telegram_message() {
 }
 
 # Clone the Sync Repo
-<<<<<<< HEAD
 repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0
 curl -o .repo/local_manifests/local_manifests.xml https://raw.githubusercontent.com/Arafattex/Local_manifest_mt6768/Lancelot/13.xml --create-dirs
 repo sync -j$(nproc --all) --force-sync
 rm -rf .repo
 df -BM
-=======
-repo init -u https://github.com/LineageOS/android.git -b lineage-20.0
-curl -o .repo/local_manifests/local_manifests.xml https://raw.githubusercontent.com/Arafattex/Local_manifest_mt6768/Lancelot/13.xml --create-dirs
-repo sync -j$(nproc --all) --force-sync
-rm -rf .repo
-
->>>>>>> bad6ea95ba8c956517e7c5325390749b3a902df2
 
 # Exit
 exit 0
