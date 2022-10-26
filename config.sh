0f01726514297cfa1942312903afa4c56307cc72
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-26 08:13:06 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export TWRP_BRANCH="twrp-12.1"
-export DT_LINK="https://github.com/Arafattex/S-Vendor-Twrp-Tree-for-mt6768 -b Lancelot_bot"
+export DT_LINK="https://github.com/Arafattex/S-Vendor-Twrp-Tree-for-mt6768 -b Merlin_c"
 
-export DEVICE="lancelot"
+export DEVICE="merlin"
 export OEM="xiaomi"
 
 # Build Target
