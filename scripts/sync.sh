10e643377a8546581fb5888421f6885510d8d81c
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-30 20:30:29 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -19,11 +19,8 @@ telegram_message() {
 # Clone the Sync Repo
 
 # Initialize local repository
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git -C .repo/manifests fetch --unshallow
-git -C .repo/manifests revert 670c209ba91d2f5ec0dab84ed2669cde71d35997
-git -C .repo/manifests revert 031f9849ea87ac1d784de96a2b8cd4322082e2f1
-git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_los .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_bootleg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Exit
