684f6989034e02d6c58cdc75997be2edf4a5a70a
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-04 20:32:41 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,18 +7,12 @@ source $CONFIG
 cd ~
 curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
 chmod a+rx /usr/local/bin/repo
-python3 -m pip install --upgrade pip
+git clone https://github.com/Arafattex/DumprX.git -b main dump
+chmod a+rx ~/dump/dumper.sh
+chmod a+rx ~/dump/setup.sh
+cd dump
+./setup.sh
+./dumper.sh https://hugeota.d.miui.com/V13.0.5.0.SJCCNXM/miui_LANCELOT_V13.0.5.0.SJCCNXM_9a2b91926f_12.0.zip
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
-
-# Clone the Sync Repo
-pip install dumpyara
-pip install aospdtgen
 # Exit
 exit 0
