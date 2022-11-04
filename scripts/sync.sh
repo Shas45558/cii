c90c32c41bfe1dc009d7666c1d50f280e753fc90
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-04 11:15:07 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -8,6 +8,7 @@ cd ~
 curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
 chmod a+rx /usr/local/bin/repo
 python3 -m pip install --upgrade pip
+sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract device-tree-compiler liblzma-dev python3-pip brotli liblz4-tool axel gawk aria2 detox cpio rename liblz4-dev curl -y
 
 # A Function to Send Posts to Telegram
 telegram_message() {
