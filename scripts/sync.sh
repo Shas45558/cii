cf0eeda3a06538aafe63cd73e8015ee0788e0e0e
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-01-12 12:22:46 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -18,9 +18,8 @@ telegram_message() {
 
 # Clone the Sync Repo
 # Initialize local repository
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_corvus .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 # Exit
 exit 0
