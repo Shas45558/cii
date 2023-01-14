6d7add70af804447b210d723e3e6f7a749a2e782
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-01-14 13:18:05 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -14,13 +14,15 @@ telegram_message() {
 	-d text="$1"
 }
 
-# Clone clang
-git clone --depth=1  $CLANG_SYNC -b $CLANG_BRANCH toolchains/proton-clang
-
-
 # Clone Kernel
 git clone --depth=1  $KT_LINK -b a13r kernel
 
+# Clone clang
+git clone --depth=1  $CLANG_SYNC -b $CLANG_BRANCH toolchains/proton-clang
+mkdir toolchains/proton-clang
+cd toolchains/proton-clang
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-13.0.0_r20/clang-r450784d.tar.gz
+tar -zxvf clang-r450784d.tar.gz
 
 # Exit
 exit 0
