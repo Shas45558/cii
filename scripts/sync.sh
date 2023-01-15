e877c2d9485cd2ce805529c6dc1c514e95e62487
Your Name <you@example.com>
2023-01-15 08:46:40 +0600

    Add

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -19,10 +19,16 @@ git clone --depth=1  $KT_LINK -b a13r kernel
 
 # Clone clang
 
-mkdir toolchains/proton-clang
-cd toolchains/proton-clang
-wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-13.0.0_r20/clang-r450784d.tar.gz
-tar -zxvf clang-r450784d.tar.gz
+mkdir clang-llvm
+mkdir gcc64-aosp
+mkdir gcc32-aosp
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-qpr3-release/clang-r383902b1.tar.gz
+tar -C ~/clang-llvm/ -zxvf clang-r383902b1.tar.gz
+wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-11.0.0_r35.tar.gz
+tar -C ~/gcc64-aosp/ -zxvf android-11.0.0_r35.tar.gz
+wget http://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-11.0.0_r34.tar.gz
+tar -C ~/gcc32-aosp/ -zxvf android-11.0.0_r34.tar.gz
+#git clone https://github.com/rama982/AnyKernel3 --depth=1 "$OUTDIR"/AnyKernel
 
 # Exit
 exit 0
