dcbe3a757589ac54b36364cd755c07afa5e42061
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-01-16 07:26:01 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -21,7 +21,6 @@ repo sync
 # Clone Trees
 DT_PATH="device/${OEM}/${DEVICE}"
 git clone $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
-
-
+git clone --depth=1 https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 -b lineage-20 kernel/xiaomi/mt6768
 # Exit
 exit 0
