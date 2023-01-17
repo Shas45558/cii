b9a0c381e3e9bae892fe11d69a2b322510d578be
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-01-17 20:10:40 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -22,7 +22,7 @@ repo sync
 DT_PATH="device/${OEM}/${DEVICE}"
 git clone $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
 git clone --depth=1 https://github.com/Arafattex/oc_a13r -b twrp kernel/xiaomi/mt6768
-got clone --depth=1 https://gitlab.com/ImSurajxD/clang-r450784d.git -b master prebuilts/clang/host/linux-x86/clang-r450784d
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-11.0.0_r29 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+git clone --depth=1 https://gitlab.com/ImSurajxD/clang-r450784d.git -b master prebuilts/clang/host/linux-x86/clang-r450784d
+# git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-11.0.0_r29 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 # Exit
 exit 0
