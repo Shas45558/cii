049be812a98e5b9e83b2848d2a19462fa55ddc8e
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-01-18 08:53:16 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -18,17 +18,6 @@ telegram_message() {
 git clone --depth=1  $KT_LINK -b a13r kernel
 
 # Clone clang
-
-mkdir clang-llvm
-mkdir gcc64-aosp
-mkdir gcc32-aosp
-wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-qpr3-release/clang-r383902b1.tar.gz
-tar -C ~/clang-llvm/ -zxvf clang-r383902b1.tar.gz
-wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-11.0.0_r35.tar.gz
-tar -C ~/gcc64-aosp/ -zxvf android-11.0.0_r35.tar.gz
-wget http://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-11.0.0_r34.tar.gz
-tar -C ~/gcc32-aosp/ -zxvf android-11.0.0_r34.tar.gz
-#git clone https://github.com/rama982/AnyKernel3 --depth=1 "$OUTDIR"/AnyKernel
-
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master toolchains/proton-clang
 # Exit
 exit 0
