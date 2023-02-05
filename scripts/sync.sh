de1d7b7f9500695d35fea941526c75b44d755932
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-02-05 15:44:35 +0600

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -23,5 +23,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_crdroida13r .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # Exit
 exit 0
