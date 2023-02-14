ef46260337c5cdd86d26bf338d39f0fcfaa65527
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-02-14 12:29:47 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 # Device 
-export TWRP_BRANCH="twrp-11"
-export DT_LINK="https://github.com/Arafattex/a13r_recovery.git -b merlinx_a13r"
+export TWRP_BRANCH="twrp-11m"
+export DT_LINK="https://github.com/Arafattex/a13r_recovery.git -b lancelot_a13r"
 
-export DEVICE="merlinx"
+export DEVICE="lancelot"
 export OEM="xiaomi"
 
 # Build Target
